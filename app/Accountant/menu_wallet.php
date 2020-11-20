bceb54bb35c7ccb81090e08cbfd66f963418df8f
Sales Queen <gowthamatspace@gmail.com>
2020-11-20 18:50:45 +0530

    Adding accountant

---
+++ app/Accountant/menu_wallet.php
@@ -0,0 +1,287 @@
+<?php 
+
+  error_reporting(0);
+
+  include '../Php/main.php';
+
+  //session handelling
+  $session=new Session();
+  $session->check_session("Admin");
+  
+  //creating user object
+  $user=new Admin();
+
+  //fetching main
+  //fetching agent result set
+  $agent_result_set=$GLOBALS['user']->read_all_agent();
+
+  //form handelling
+  if(isset($_POST['download_excel'])){
+      //preparing value array
+      $values_array=array();
+      $policy_result_set=$GLOBALS['user']->read_all_policy();
+      $i=0;
+      while($policy_result=$policy_result_set->fetch_assoc()){
+          //comission calculation
+          $comission=0;
+          if($policy_result['comission_type']=='OD' && $policy_result['payment_mode']=='Cash'){
+            $comission=$policy_result['od_premium']*($policy_result['comission_percentage']/100);
+          }
+          if($policy_result['comission_type']=='NP' && $policy_result['payment_mode']=='Cash'){
+            $comission=$policy_result['net_premium']*($policy_result['comission_percentage']/100);
+          }
+           $values_array[$i]=array($policy_result['issue_date'],$policy_result['policy_number'],$policy_result['policy_type'],$policy_result['product'],$policy_result['company_name'],$policy_result['policy_number'],
+           $policy_result['customer_name'],$policy_result['total_premium'],$comission,$policy_result['payment_mode'],get_wallet_amount($policy_result['agent_id']),get_branch($policy_result['agent_id']),get_agent_name($policy_result['agent_id']));
+           $i++;
+      }
+      //call to download excel
+      $GLOBALS['user']->excel(array("Policy Date","Policy Number","Policy Type","Product","Company","Policy Number","Customer Name","Total Premium","Payable Amount",
+      "Payment Mode","Balance","Branch","Agent Name"),$values_array);
+      header("Location:menu_wallet.php");
+  }
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Wallet</title>
+
+  <!-- CSS -->
+  <!--Bootstrap-->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+  
+  <!--Fonts-->
+  <!--Roboto-->
+  <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;900&display=swap" rel="stylesheet">
+  <!--Open Sans-->
+  <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;600&display=swap" rel="stylesheet">
+  <!--Montserrat-->
+  <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@200;300;400&display=swap" rel="stylesheet">
+
+  <!--Custom style sheet-->
+  <link rel="stylesheet" href="../styles/main.css">
+</head>
+<body>
+    <header>
+        <div class="container">
+            <nav class="navbar navbar-dark justify-content-between">
+                <a class="navbar-brand">ROYAL BROKERS</a>
+                <div id="account">
+                    <p><?php echo $_SESSION['name']?></p>
+                    <div id="account-menu">
+                        <img src="../images/sign_in_side.jpg" alt="">
+                    </div>
+                </div>
+            </nav>
+        </div>
+    </header>
+    <section id="navbar">
+        <div class="container">
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav mr-auto">
+                        <li class="nav-item">
+                            <a class="nav-link" href="menu_dashboard.php">Dashboard</a>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Master</a>
+                            <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                                <a class="dropdown-item" href="menu_master_company.php">Company</a>
+                                <a class="dropdown-item" href="menu_master_company_code.php">Company Code</a>
+                                <a class="dropdown-item" href="menu_master_policy_period.php">Policy Period</a>
+                                <a class="dropdown-item" href="menu_master_policy_type.php">Policy Type</a>
+                                <a class="dropdown-item" href="menu_master_product.php">Product</a>
+                            </div>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Manage User</a>
+                            <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                                <a class="dropdown-item" href="menu_manage_user_branch_manager.php">Branch Manager</a>
+                                <a class="dropdown-item" href="menu_manage_user_operator.php">Operator</a>
+                                <a class="dropdown-item" href="menu_manage_user_accountant.php">Accoutant</a>
+                                <a class="dropdown-item" href="menu_manage_user_agent.php">User</a>
+                                <a class="dropdown-item" href="menu_manage_user_create_branch.php">Create Branch</a>
+                            </div>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="menu_policy.php">Policy</a>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Utilities</a>
+                            <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                                <a class="dropdown-item" href="menu_utilities_comission_recivable.php">Comission Recivable</a>
+                                <a class="dropdown-item" href="menu_utilities_comission_payable.php">Comission Payable</a>
+                                <a class="dropdown-item" href="menu_utilities_cheque_status.php">Cheque Status</a>
+                                <a class="dropdown-item" href="menu_utilities_cash_recived.php">Cash Recived</a>
+                                <a class="dropdown-item" href="menu_utilities_cash_paid.php">Cash Paid</a>
+                            </div>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link active" href="menu_wallet.php">Wallet</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="../logout.php" id="logout-link">Logout</a>
+                        </li>
+                    </ul>
+                </div>
+          </nav>
+        </div>
+    </section>
+    <section id="main-container">
+      <div class="container">
+        <div class="form-container">
+          <div class="row">
+            <div class="col-md-12">
+
+                <div class="row">
+                    <div class="col-md-6">
+                        <h2>Wallet</h2>
+                    </div>
+                    <div class="col-md-6">
+                        <form action="<?php echo $_SERVER['PHP_SELF']?>" method="POST" style="float:right">
+                            <input type="submit" name="download_excel" value="Download Excel">
+                        </form>
+                    </div>
+                </div>
+                
+                <ul class="nav nav-tabs">
+                    <li><a data-toggle="tab" href="#wallet" class="active">Wallet</a></li>
+                </ul>
+
+                <div class="tab-content">
+                    <div id="wallet" class="tab-pane fade show in active">
+                        <div class="row filter">
+                            <div class="col-sm-12">
+                                <div class="search-container">
+                                    <form action="" method="POST">
+                                        <input id="search_1" type="text" placeholder="Search" name="search">
+                                    </form>
+                                </div>
+                            </div> 
+                        </div>
+                        <div class="table-scroll">
+                            <table id="table_1" class="table table-bordered">
+                                <thead>
+                                    <tr>
+                                        <th>Agent Name</th>
+                                        <th>Branch</th>
+                                        <th>Balance</th>
+                                        <th colspan="2">Transaction</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <?php
+                                        if($agent_result_set){
+                                            while($agent_result=$agent_result_set->fetch_assoc()){
+                                                echo "<tr>";
+                                                echo "  <td>".$agent_result['name']."</td>";
+                                                echo "  <td>".$user->get_branch($agent_result['id'])."</td>";
+                                                echo "  <td>".$user->get_wallet_amount($agent_result['id'])."</td>";
+                                                echo '  <td><a href="view_ten_transaction.php?agent_id='.$agent_result['id'].'"><button>Mini Statement</button></a></td>';
+                                                echo '  <td><a href="view_transaction.php?agent_id='.$agent_result['id'].'"><button>Statement</button></a></td>';
+                                                echo "</tr>";
+                                            }
+                                        }else{
+                                            echo "<tr>No records found</tr>";
+                                        }
+                                    ?>
+                                </tbody>
+                            </table>
+                        </div>
+                        <!--<nav aria-label="Page navigation">
+                            <ul class="pagination">
+                                <li class="page-item"><a class="page-link" href="#">Previous</a></li>
+                                <li class="page-item"><a class="page-link" href="#">1</a></li>
+                                <li class="page-item"><a class="page-link" href="#">2</a></li>
+                                <li class="page-item"><a class="page-link" href="#">3</a></li>
+                                <li class="page-item"><a class="page-link" href="#">Next</a></li>
+                            </ul>
+                        </nav>-->
+                    </div>
+                </div>
+  
+          </div>
+        </div>
+      </div>
+    </section>
+    <footer>
+
+    
+    </footer>
+    <div id='overlay'>
+        <div class="container">
+          <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="POST">
+            <div class="row close_container">
+                <span class="fa fa-times" onclick="close_overlay()"></span>
+            </div>
+            <div class="row">
+                <div class="col-md-4">
+                    <label for="agent_name">Agent</label>
+                    <select onchange="update_tp_policy_end_date()" name="tp_policy_period" class="form-control" id="tp_policy_period" required="required">
+                        <option value="<?php echo $result['tp_policy_period'];?>"><?php echo $result['tp_policy_period'];?></option>
+                    </select>
+                    <input type="text" id="agent_name" name="agent_name" placeholder="Agent Name" class="form-control" value="" required="required">
+                </div>    
+            </div>
+            <div class="row">
+                <div class="col-md-4">
+                    <input type="submit" value="Approve" name="approve_submit">
+                </div>
+            </div>
+          </form>
+        </div>
+    </div>
+    <!--Message-->
+    <div class="alert hide">
+        <span class="fas fa-exclamation-circle"></span>
+        <span class="msg" id="message"></span>
+        <div class="close-btn">
+          <span class="fa fa-times"></span>
+        </div>
+    </div>
+
+    <!-- jQuery and JS bundle w/ Popper.js -->
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+    <!--Font awesome-->
+    <script src="https://use.fontawesome.com/793bc63e83.js"></script>
+  <style>
+    .fa{
+        font:normal normal normal 20px/1 FontAwesome;
+    }
+  </style>
+    <!--Custom script-->
+    <script src="../scripts/search.js"></script>
+    <script src="../scripts/main.js"></script>
+    <?php 
+    //Message handelling
+    if(isset($_SESSION['message'])){
+        echo "<script>
+                $('.alert').addClass('show');
+                $('#message').text('".$_SESSION['message']."');
+                $('.alert').removeClass('hide');
+                $('.alert').addClass('showAlert');
+                $('.alert').css('opacity','1');
+                setTimeout(function(){
+                    $('.alert').removeClass('show');
+                    $('.alert').addClass('hide');
+                    $('.alert').css('opacity','0');
+                },5000);
+            </script>";
+        unset($_SESSION['message']);
+    }else{
+        echo $_SESSION['message'];
+    }
+  ?>
+</body>
+</html>
+
+                        
+
+
+                        
